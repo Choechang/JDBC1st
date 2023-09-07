1e995e845b5fa9116e180dd60d6caa604db94f75
Choechang <dame9735@naver.com>
2023-09-07 16:47:57 +0900

    .

---
+++ JDBC_PreparedStatement_02/src/com/kh/model/vo/Member.java
@@ -0,0 +1,163 @@
+package com.kh.model.vo;
+
+import java.sql.Date;
+
+/**
+ * 
+ * VO(value object)
+ *	한 명의 회원(db테이블의 한 행의 데이터)에 대한 데이터를 기록할 수 있는 저장용 객체
+ */
+public class Member {
+	//필드
+	//필드는 기본적으로 DB컬럼명과 유사하게 작업.
+	private int userNo;
+	private String userId;
+	private String userPsw;
+	private String userName;
+	private String gender;
+	private int age;
+	private String email;
+	private String phone;
+	private String address;
+	private String hobby;
+	private Date enrollDate; // java.sqw.Date import
+	
+	
+	public Member() {
+		super();
+	}
+	
+	//생성자 alt + shft + s => o 
+	public Member(int userNo, String userId, String userPsw, String userName, String gender, int age, String email,
+			String phone, String address, String hobby, Date enrollDate) {
+		super();
+		this.userNo = userNo;
+		this.userId = userId;
+		this.userPsw = userPsw;
+		this.userName = userName;
+		this.gender = gender;
+		this.age = age;
+		this.email = email;
+		this.phone = phone;
+		this.address = address;
+		this.hobby = hobby;
+		this.enrollDate = enrollDate;
+	}
+
+	public Member(String userId, String userPsw, String userName, String gender, int age, String email, String phone,
+			String address, String hobby) {
+		super();
+		this.userId = userId;
+		this.userPsw = userPsw;
+		this.userName = userName;
+		this.gender = gender;
+		this.age = age;
+		this.email = email;
+		this.phone = phone;
+		this.address = address;
+		this.hobby = hobby;
+	}
+	
+	//getter/setter alt + shift s r
+
+	public int getUserNo() {
+		return userNo;
+	}
+
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserPsw() {
+		return userPsw;
+	}
+
+	public void setUserPsw(String userPsw) {
+		this.userPsw = userPsw;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getHobby() {
+		return hobby;
+	}
+
+	public void setHobby(String hobby) {
+		this.hobby = hobby;
+	}
+
+	public Date getEnrollDate() {
+		return enrollDate;
+	}
+
+	public void setEnrollDate(Date enrollDate) {
+		this.enrollDate = enrollDate;
+	}
+
+	@Override
+	public String toString() {
+		return  userNo + ", " + userId + ", " + userPsw + ", " + userName
+				+ ", " + gender + ", " + age + ", " + email + ", " + phone + ", "
+				+ address + ", " + hobby + ", " + enrollDate;
+	}
+	
+	
+		
+	//메소드
+	
+
+}
